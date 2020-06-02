c9858bd99c4a1cfa94ff9b8281e579c0f49b90e6
Julien RISACHER <julien.risacher@pc-scol.fr>
2020-06-02 12:30:56 +0200

    changed title

--- premiere-application/src/app/app.component.ts
+++ premiere-application/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'premiere-application';
+  title = 'Gestion des naissances de Wookies';
 }
